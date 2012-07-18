5c8e8b0dd05dc6a412ee786641546227a5d82e47
trironk <trironk@gmail.com>
2012-07-18 16:53:28 -0400

    Added helper methods to ConvertHelper for converting bytes to a hex string representation and back.
    Added unit tests for ConvertHelper.

--- DotNetUtilityLibrary/ConvertHelper.cs
+++ DotNetUtilityLibrary/ConvertHelper.cs
@@ -20,5 +20,20 @@ namespace DotNetUtilityLibrary
 			System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
 			return new string(chars);
 		}
+
+		public static byte[] HexStringToBytes(string str)
+		{
+			int NumberChars = str.Length;
+			byte[] bytes = new byte[NumberChars / 2];
+			for (int i = 0; i < NumberChars; i += 2)
+				bytes[i / 2] = Convert.ToByte(str.Substring(i, 2), 16);
+			return bytes;
+		}
+
+		public static string BytesToHexString(byte[] bytes)
+		{
+			string str = BitConverter.ToString(bytes);
+			return str.Replace("-", "");
+		}
 	}
 }
---
+++ Unit Testing/UT_ConvertHelper/UT_ConvertHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using DotNetUtilityLibrary;
+
+namespace UT_ConvertHelper
+{
+	[TestFixture]
+	public class UT_TypeHelper
+	{
+		[Test]
+		public void test_string()
+		{
+			string testString = "Hello world. This is a testing string.";
+			Assert.AreEqual(testString, ConvertHelper.BytesToHexString(ConvertHelper.HexStringToBytes(testString)));
+			Assert.AreEqual(testString, ConvertHelper.BytesToString(ConvertHelper.StringToBytes(testString)));
+		}
+	}
+}
